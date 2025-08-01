6f3057cf5ef5867f9ea5026b6ec7bddd55d0b8e6
foamyguy <foamyguy@gmail.com>
2025-08-01 10:25:32 -0500

    add active3 rbg pinout

--- src/include/piomatter/pins.h
+++ src/include/piomatter/pins.h
@@ -96,4 +96,24 @@ struct active3_pinout_bgr {
     static constexpr uint32_t post_addr_delay = 5;
 };
 
+struct active3_pinout_rbg {
+    static constexpr pin_t PIN_RGB[] = {11, 7, 27,  8,  10, 9, 12,  6, 5,
+                                        19, 20, 13, 14, 3, 2, 26, 21, 16};
+    static constexpr pin_t PIN_ADDR[] = {22, 23, 24, 25, 15};
+    static constexpr pin_t PIN_OE = 18;  // /OE: output enable when LOW
+    static constexpr pin_t PIN_CLK = 17; // SRCLK: clocks on RISING edge
+    static constexpr pin_t PIN_LAT = 4;  // RCLK: latches on RISING edge
+
+    static constexpr uint32_t clk_bit = 1u << PIN_CLK;
+    static constexpr uint32_t lat_bit = 1u << PIN_LAT;
+    static constexpr uint32_t oe_bit = 1u << PIN_OE;
+    static constexpr uint32_t oe_active = 0;
+    static constexpr uint32_t oe_inactive = oe_bit;
+
+    static constexpr uint32_t post_oe_delay = 0;
+    static constexpr uint32_t post_latch_delay = 0;
+    static constexpr uint32_t post_addr_delay = 5;
+};
+
+
 } // namespace piomatter
